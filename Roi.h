55c6bbc7230abc56f9c570f1d41702c8b0b30968
AshKGolam <90001285+AshKGolam@users.noreply.github.com>
2022-04-24 15:17:57 -0400

    ExceptionKing

--- Roi.h
+++ Roi.h
@@ -8,20 +8,30 @@ namespace modele
 class Roi: public Piece
 {
 public:
-     Roi(int x, int y, bool couleur ):Piece::Piece(x, y, couleur)
+
+     Roi(int x, int y, bool couleur )
      {
          couleur_ = couleur;
+         count_++;
+
+         if (count_ >= 3)
+         {
+             throw("Plus de deux Rois non permis!");
+         }
 
      }
      virtual void mouvementLegal(Echequier& echiquier );
      virtual void deplacement(Echequier& echiquier );
      bool miseEnEchec(Echequier& echiquier);
+     static int getCount();
+
 
      bool echec = false;
 
 private:
-    const int MAXMOUVEMENTX = 1;
-    const int MAXMOUVEMENTY = 1;
+    static int count_;
+    const int MAXMOUVEMENTX_ = 1;
+    const int MAXMOUVEMENTY_ = 1;
 
 };
 
