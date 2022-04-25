48ecec4ee1e24631d4a29e266f14486a4a5f377f
Ryan Lahbabi <89122986+ryanLahbabi@users.noreply.github.com>
2022-04-25 15:05:26 -0500

    Ajout commentaires TD6

--- Piece.h
+++ Piece.h
@@ -1,3 +1,18 @@
+/*
+Partie 1: Nos deux namespace différents sont namespace modele( On le retrouve dans Piece.h, Roi.h, Tour.h, echequier.h, Regle.h, jeu.h )
+ et namespace vue(on le retrouvedans mainwindow.h).
+
+Partie 2: Dans notre fichier Roi.h vous trouverez le compteur d'instances de roi et  l'excpetion lancée au cas où
+ il y a plus de deux roi avec le message d'erreur. Nous voulions implémenter l'exception dans Roi.cpp mais cela nous donnait des erreurs de compilation
+
+Partie 3: La classe d'implémentation du RAII est dans Piece.cpp, vous y trouverez les 3 étapes demandée soit ajouter une Piece temporairement,
+vérifier la mise en échec, enlever la piece à l'aide du shared_ptr qui détruira automatiquement la piece grâce aux pointeurs intelligents
+ et enfin le retour du test mise en échec. Malheureusement, nous avons dû commenter nos deux fonction ajouter et enlever piece car ces deux
+fonctions proviennet de echequier.h et nous devons régler un problème de compilation à cause de l'inclusion de nos en-têtes
+*/
+
+
+
 #ifndef PIECE_H
 #define PIECE_H
 
